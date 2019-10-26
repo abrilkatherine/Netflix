d919e403fb05195921844cd3882e4f39db654e2b
Abril <abrilkatherine@hotmail.com>
2019-10-25 21:48:53 -0300

    Los dos get

--- src/main/java/Controller/ContentController.java
+++ src/main/java/Controller/ContentController.java
@@ -2,6 +2,7 @@ package Controller;
 
 import Model.Content;
 import Service.ContentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +11,18 @@ import java.util.List;
 
 @RestController
 public class ContentController {
+
+    @Autowired
     private ContentService contentService;
     private ContentController(ContentService contentService){
     this.contentService= contentService;
-
     }
 
     @GetMapping("/content")
     public List<Content>content(String title){
             return contentService.contents(title);
     }
+    public List<Content>contents(@RequestParam(value="genre", required= false)String genre){
+        return contentService.contents(genre);
+   }
 }
-
---
+++ src/main/java/Service/OmdbService.java
@@ -0,0 +1,8 @@
+package Service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class OmdbService {
+
+}
