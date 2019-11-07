f9fdbe14d851ec3abf69dde9be7ec23e8517ed35
daanimelian <meliandaani@gmail.com>
2019-11-06 21:57:59 -0300

    intentando hacer los test

---
+++ src/test/java/ada/septima/back/service/UserServiceTest.java
@@ -0,0 +1,33 @@
+/*package ada.septima.back.service;
+
+import ada.septima.back.model.ContenidoUnitario;
+import ada.septima.back.model.Content;
+import ada.septima.back.model.User;
+import ada.septima.back.model.Usuario;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class UserServiceTest {
+    private Usuario personita;
+    private String willy;
+    private ContenidoUnitario titanic = mock(ContenidoUnitario.class);
+    private ContenidoUnitario matrix = mock(ContenidoUnitario.class)
+    @BeforeEach
+    public void setUp(){ personita= new Usuario(willy);}
+
+    @Test
+    public void contenidoVisto(){
+
+        assertEquals(titanic, personita.getContenidoUnitarioVisto());
+
+
+    }*/
+
+
+
+
+}
