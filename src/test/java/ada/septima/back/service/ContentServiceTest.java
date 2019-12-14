6a94dd2db05ddbefd7c5430480e7134a109866fe
daanimelian <meliandaani@gmail.com>
2019-12-13 21:27:10 -0300

    que sea lo que tenga que ser

--- src/test/java/ada/septima/back/service/ContentServiceTest.java
+++ src/test/java/ada/septima/back/service/ContentServiceTest.java
@@ -12,10 +12,14 @@ public class ContentServiceTest {
     ContentStorage contentStorage = mock(ContentStorage.class);
     RestClientStorage restClientStorage = mock(RestClientStorage.class);
     ContentService contentService = new ContentService(contentStorage, restClientStorage);
+    Content titanic = new Content(123, "titanic", 1997, 145, "drama", "pepe", null);
+    List<Content> contentFromJson = new ArrayList();
+
     @Test
     public void contentsTest(){
+        contentFromJson.add(titanic)
         when(contentStorage.readContent());
-
+        contentService.content("titanic");
 
     }
 }
